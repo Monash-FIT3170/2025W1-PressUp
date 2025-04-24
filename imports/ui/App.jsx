6d11e166138576b0ede4d7a2caadd640fc564510
ekeyhoe <ekeyhoe@gmail.com>
2025-04-24 16:03:59 +1000

    Edit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,8 +6,12 @@ import { Sidebar } from './Components/Sidebar.jsx';
 export const App = () => (
   <div className='app-container'>
     <Sidebar />
+    
     <div className="main-content" style={{ marginLeft: '80px' }}>
+       {/* Page title */}
       <h1>Inventory</h1>
+      
+       {/* Page content */}
       <Hello />
       <Info /> 
     </div>
