afd871580b70de9a6ac6938bb716617e00c6723f
Khoi Nguyen <khoinm2003@gmail.com>
2025-04-27 12:38:18 +1000

    Feat: Build dummy inventory table

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
-import { Sidebar } from './Components/Sidebar.jsx';
+import React, { useState, useEffect, useRef } from "react";
+import { Sidebar } from "./Components/Sidebar.jsx";
+import { IngredientTable } from "./Components/IngredientTable/IngredientTable.jsx";
 
-export const App = () => (
-  <div className='app-container'>
-    <Sidebar />
-    
-    <div className="main-content" style={{ marginLeft: '80px' }}>
-       {/* Page title */}
-      <h1>Inventory</h1>
-      
-       {/* Page content */}
-      <Hello />
-      <Info /> 
+export const App = () => {
+  const [openOverlay, setOpenOverlay] = useState(null);
+  const overlayRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (overlayRef.current && !overlayRef.current.contains(event.target)) {
+        setOpenOverlay(null);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  return (
+    <div className="app-container">
+      <Sidebar />
+      <div className="main-content" style={{ marginLeft: "80px" }}>
+        <h1>Inventory</h1>
+        <IngredientTable
+          openOverlay={openOverlay}
+          setOpenOverlay={setOpenOverlay}
+          overlayRef={overlayRef}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
