b8e2d3a306106ddfbddcbb066298d3ab82f8493a
kbis0003 <kbis0003@student.monash.edu>
2025-04-30 09:04:53 +1000

    update imports.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import {InventoryCollection} from '/imports/api/inventory/inventory-collection';
+import "../imports/api/inventory/inventory-publications";
+import "../imports/api/inventory/inventory-methods";
 
 
 Meteor.startup(async () => {
