22ea5e41dbe912a7f9512cbcda519c307e5a6806
kbis0003 <kbis0003@student.monash.edu>
2025-04-30 00:41:34 +1000

    add publication for getting items based off name. it will look for items containing a substring.

--- imports/api/inventory/inventory-publications.js
+++ imports/api/inventory/inventory-publications.js
@@ -4,3 +4,10 @@ import { InventoryCollection } from "./inventory-collection";
 Meteor.publish("Inventory.all",()=> {
     return InventoryCollection.find();
 })
+
+Meteor.publish("Inventory.nameIncludes", (subString) => {
+    if (subString.length > 0) {
+        return InventoryCollection.find({name:{$regex:subString, $options: 'i'}});
+    }
+    return InventoryCollection.find();
+})
