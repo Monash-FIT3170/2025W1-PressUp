04c8523ca00b1344dc72a6e26f525fb6fb8d49c6
Connor Saunders <csau0007@student.monash.edu>
2025-05-07 17:13:50 +1000

    added nameIncludes commit

--- imports/api/suppliers/SuppliersPublications.js
+++ imports/api/suppliers/SuppliersPublications.js
@@ -4,3 +4,12 @@ import { SuppliersCollection } from "./SuppliersCollection";
 Meteor.publish("Suppliers", () => {
   return SuppliersCollection.find();
 });
+
+Meteor.publish("suppliers.nameIncludes", (subString) => {
+  if (subString.length > 0) {
+    return SuppliersCollection.find({
+      name: { $regex: subString, $options: "i" },
+    });
+  }
+  return SuppliersCollection.find();
+});
