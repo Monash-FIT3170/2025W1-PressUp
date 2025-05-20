02e03da37a5d032af15c09b94d5ed9aaa3796422
Hugo Ferguson <hfer0009@student.monash.edu>
2025-05-21 09:13:22 +1000

    Added the schedular to server.

--- server/scheduler.js
+++ server/scheduler.js
@@ -3,22 +3,30 @@ import { applyScheduledChange } from '../imports/api/scheduled-changes/apply-cha
 
 const ONE_MINUTE_MS = 60 * 1000;
 
-Meteor.startup(() => {
-  Meteor.setInterval(() => {
-    const NOW = new Date();
+async function getApplyScheduledChanges(date) {
+	console.log(`Looking for scheduled changes at ${date}`);
 
-    ScheduledChanges.find({
-      scheduledTime: { $lte: NOW },
-      applied: false
-    }).forEach(change => {
-      try {
-        applyScheduledChange(change);
-      } 
-	  catch (err) {
-        console.error('Failed to apply change', change._id, err);
-      }
-    });
-  }, 
-  // Run every minute.
-  ONE_MINUTE_MS);
+	const changes = ScheduledChanges.find({
+		scheduledTime: { $lte: date },
+		applied: false
+	});
+
+	const count = await changes.countAsync();
+	console.log(`Found ${count} changes to apply.`);
+
+	for await (const change of changes) {
+		try {
+			applyScheduledChange(change);
+		} catch (err) {
+			console.error('Failed to apply change', change._id, err);
+		}
+	}
+}
+
+Meteor.startup(() => {
+	getApplyScheduledChanges(new Date());
+
+	Meteor.setInterval(() => {
+		getApplyScheduledChanges(new Date());
+	}, ONE_MINUTE_MS);
 });
