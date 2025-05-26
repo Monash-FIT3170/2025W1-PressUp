3e42b9e6d4e6932b3eb8f3bcaef231f566b71cd2
kbis0003 <kbis0003@student.monash.edu>
2025-05-27 01:44:45 +1000

    print receipt now creates a txt file

--- imports/ui/Components/RecieptGeneration/PrintRecieptButton/printReciept.jsx
+++ imports/ui/Components/RecieptGeneration/PrintRecieptButton/printReciept.jsx
@@ -14,31 +14,38 @@ export const PrintReciept = ({
         total -= order.discount;
     }
     var change = order.recievedPayment - total;
+    var str = "Order #" + order._id;
+    str += "\n";
+    str += "Table " + order.table;
+    str += "\n\n";
+    str += "Ordered Items:\n";
+    order.items.forEach(item => {
+        str += item.quantity + " " + item.menu_item + " ....................... $" + (item.quantity*item.price).toFixed(2) + "\n";
+    })
+    str += "\nsubtotal ....................... $" + gross.toFixed(2);
+    str += "\nGST ....................... $" + GST.toFixed(2);
+    if (order.discount) {
+        str += "\ndiscount ....................... -$" + order.discount.toFixed(2);
+    }
+    str += "\ntotal ....................... $" + total.toFixed(2);
+    str += "\nreceived ....................... $" + order.recievedPayment.toFixed(2);
+    str += "\nchange ....................... $" + change.toFixed(2);
+
+    
+    const file = new Blob([str], { type: 'text/plain' });
+
     function printReceiptFunction() {
-        var str = "Order #" + order._id;
-        str += "\n";
-        str += "Table " + order.table;
-        str += "\n\n";
-        str += "Ordered Items:\n";
-        order.items.forEach(item => {
-            str += item.quantity + " " + item.menu_item + " ....................... $" + (item.quantity*item.price).toFixed(2) + "\n";
-        })
-        str += "\nsubtotal ....................... $" + gross.toFixed(2);
-        str += "\nGST ....................... $" + GST.toFixed(2);
-        if (order.discount) {
-            str += "\ndiscount ....................... -$" + order.discount.toFixed(2);
-        }
-        str += "\ntotal ....................... $" + total.toFixed(2);
-        str += "\nrecieved ....................... $" + order.recievedPayment.toFixed(2);
-        str += "\nchange ....................... $" + change.toFixed(2);
         console.log(str);
     }
+
     return (
+        <a href = {URL.createObjectURL(file)} className="link" download={order._id+".txt"}>
         <div className="button">
             <button
                 className="text"
                 onClick={printReceiptFunction}
             >Print Reciept</button>
         </div>
+        </a>
     )
 }
