ff327f27ec7373ff5285510f4cfdfe43c235dee6
kbis0003 <kbis0003@student.monash.edu>
2025-05-27 11:41:20 +1000

    now displays change info upon proceeding from order summary.

--- imports/ui/Components/POS/orderSummary.jsx
+++ imports/ui/Components/POS/orderSummary.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React , {useState}   from "react";
 import { useSubscribe, useFind } from "meteor/react-meteor-data";
 import "./orderSummary.css";
 import { LoadingIndicator } from "../LoadingIndicator/LoadingIndicator.jsx";
 import { OrdersCollection } from "../../../api/orders/orders-collection";
 import { PrintReciept } from "../RecieptGeneration/PrintRecieptButton/printReciept.jsx";
+import { ChangeDetails } from "./ChangeDetails.jsx";
 
 export const OrderSummary = ({
     orderID
 }) => {
+  
+    const [showChangeInfo,setShowChangeInfo] = useState(false);
     const isLoading = useSubscribe("orders.id", orderID);
     var order = useFind(() => OrdersCollection.find({}), [orderID]);
+    
+    function proceedButtonAction() {
+      setShowChangeInfo(true);
+    }
     //if loading show loading indicator
     if (isLoading()) {
         return <LoadingIndicator />;
@@ -35,6 +42,32 @@ export const OrderSummary = ({
         }
         var change = order.recievedPayment - total;
         var isPaidFor = change > 0;
+    }
+    //show change info section after proceeding
+    if (showChangeInfo) {
+      return (
+        <div className="order-summary-section">
+          <div className="top-section">
+            <div className="order-number">Your Order</div>
+            <div className="table-number">{"Table " + order.table + "."}</div>
+          </div>
+          <div className="middle-section">
+            {order.items.map((item) => (
+              <div className="order-item" key={item.menu_item}>
+              <div className="order-item-name">
+                {item.quantity + "x" + item.menu_item}
+              </div>
+              <div className="order-item-qty">
+                {"$" + (item.price*item.quantity).toFixed(2)}
+              </div>
+            </div>
+            ))}
+          </div>
+          <div className="bottom-section">
+            <ChangeDetails order = {order} orderID= {order._id}/>
+          </div>
+      </div>
+      );
     }
     //display order summary
     return (
@@ -104,7 +137,12 @@ export const OrderSummary = ({
             </div>
         </div>) : (<></>)
         }
-        <PrintReciept order={order}/>
+        <div className="button-div">
+            <button
+                className="button"
+                onClick={proceedButtonAction}
+            >Proceed</button>
+        </div>
         </div>
     </div>
     );
