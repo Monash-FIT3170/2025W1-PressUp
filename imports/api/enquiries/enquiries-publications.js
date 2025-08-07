f4e0d6b02987cff2b9de1476d36403ffa281c6a3
kbis0003 <kbis0003@student.monash.edu>
2025-08-08 08:39:07 +1000

    enquiries

--- imports/api/enquiries/enquiries-publications.js
+++ imports/api/enquiries/enquiries-publications.js
@@ -2,7 +2,7 @@ import { Meteor } from "meteor/meteor";
 import { EnquiriesCollection } from "./enquiries-collection";
 
 Meteor.publish("enquiries.all", () => {
-  return EnquiriesCollection.find();
+  return EnquiriesCollection.find({},{sort: {date_created: -1}});
 });
 
 Meteor.publish("enquiries.active", () => {
