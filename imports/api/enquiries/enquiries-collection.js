1e3d576cb5faad483d3067a42762f6f6023ad7bf
kbis0003 <kbis0003@student.monash.edu>
2025-08-08 08:37:16 +1000

    enquiries now have a date attribute.

--- imports/api/enquiries/enquiries-collection.js
+++ imports/api/enquiries/enquiries-collection.js
@@ -6,6 +6,7 @@ export const EnquiriesCollection = new Mongo.Collection("enquiries");
 EnquiriesCollection.schema = new SimpleSchema({
   content: { type: String },
   contact: { type: String },
+  date: {type: Date},
   active: {type: Boolean, defaultValue: true},
   response: {type: String, optional: true},
   customerName: {type: String,optional:true}});
