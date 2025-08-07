4971eb8b1dbc2cb05ec5ed89dde3e048a232c647
kbis0003 <kbis0003@student.monash.edu>
2025-08-08 01:31:09 +1000

    add collection for enquiries.

---
+++ imports/api/enquiries/enquiries-collection.js
@@ -0,0 +1,11 @@
+import { Mongo } from "meteor/mongo";
+import SimpleSchema from "simpl-schema";
+
+export const EnquiriesCollection = new Mongo.Collection("enquiries");
+
+EnquiriesCollection.schema = new SimpleSchema({
+  content: { type: String },
+  contact: { type: String },
+  active: {type: Boolean, defaultValue: true},
+  response: {type: String, optional: true},
+  customerName: {type: String,optional:true}});
