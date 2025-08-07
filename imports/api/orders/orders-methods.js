b7d3452b746741476f85431b34a98c477ccc1427
Jamie <jamiegatt.jg@gmail.com>
2025-08-07 10:53:18 +1000

    Implement order insertion with ingredient cost calculation (not working)

--- imports/api/orders/orders-methods.js
+++ imports/api/orders/orders-methods.js
@@ -1,9 +1,37 @@
 import {Meteor} from 'meteor/meteor';
 import { OrdersCollection } from "./orders-collection";
+import { Menu } from '/imports/api/menu/menu-collection';
+import { InventoryCollection } from '/imports/api/inventory/inventory-collection';
 
 Meteor.methods({
 
     async 'orders.insert'(order) {
+        // unable to properly obtain the menu item data (obviously cant obtain inventory item data then cost either))
+        const orderItemsWithCosts = [];
+
+        for (const item of order.items) {
+            const menuItem = await Menu.findOneAsync({ _id: item._id });
+            if (!menuItem) {
+                throw new Meteor.Error('menu-item-not-found', `Menu item with ID ${item._id} not found.`);
+            }
+
+            const ingredients = await InventoryCollection.find({
+                _id: { $in: menuItem.ingredients || [] }
+                }).fetchAsync();
+
+
+            const ingredientCost = ingredients.reduce((sum, ingredient) => sum + (ingredient.price || 0), 0);
+
+            orderItemsWithCosts.push({
+                ...item,
+                ingredientCost: ingredientCost
+            });
+        }
+
+        const orderWithCosts = {
+            ...order,
+            items: orderItemsWithCosts
+        };
         return await OrdersCollection.insertAsync(order);
     },
 
