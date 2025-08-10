02c28db0d4f886392bfb738089b283609b20a461
kbis0003 <kbis0003@student.monash.edu>
2025-08-10 23:56:59 +1000

    update enquiries to match look of other pre login components.

--- imports/ui/Components/Enquiries/Enquiries.jsx
+++ imports/ui/Components/Enquiries/Enquiries.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import './Enquiries.css';
+import { CustomerNavbar } from '../PreLogin/CustomerNavbar.jsx';
+import { useNavigate } from 'react-router-dom';
 
 export const Enquiries = () => {
-    const [contact,setContact] = useState('');
-    const [content,setContent] = useState('');
+    const [contact,setContact] = useState("");
+    const [content,setContent] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate('/');
+      };
 
     const checkIsEmail = (string) => {
         emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
@@ -20,41 +28,56 @@ export const Enquiries = () => {
             });
             setContact('');
             setContent('');
+            navigate('/')
         }
     }
 
     return (
+        <div className='pre-login-container'>
+        <CustomerNavbar />
+        
         <div className="enquiries-container">
-            <form onSubmit={handleSubmit} className="enquiries-form">
-                <div className="enquiries-input-group">
-                    <label htmlFor="contact">Email</label>
-                    <input
-                        id="contact"
-                        type="email"
-                        value={contact}
-                        onChange={(e) => setContact(e.target.value)}
-                        placeholder="Enter Email"
-                        required
-                        disabled={isLoading}
-                    />
+            <div className='support-card'>
+                <div className='enquiries-header'>
+                <button onClick={handleGoBack} className="back-btn">
+                ← Back to Home
+                </button>
+                    <h1>Customer Support</h1>
+                    <p>Provide us with an email address to contact and one of our team members will respond as soon as possible!</p>
                 </div>
-                <div className="enquiries-input-group">
-                    <label htmlFor="content">How can we help you?</label>
-                    <textarea
-                        id="content"
-                        type="text"
-                        value={content}
-                        onChange={(e) => setContent(e.target.value)}
-                        required
-                        className='enquiries-text-box'
+                <form onSubmit={handleSubmit} className="enquiries-form">
+                    <div className="enquiries-input-group">
+                        <label htmlFor="contact">Email Address *</label>
+                        <input
+                            id="contact"
+                            type="email"
+                            value={contact}
+                            onChange={(e) => setContact(e.target.value)}
+                            placeholder="example@email.com"
+                            required
+                            disabled={isLoading}
+                        />
+                    </div>
+                    <div className="enquiries-input-group">
+                        <label htmlFor="content">How can we help you? *</label>
+                        <textarea
+                            id="content"
+                            type="text"
+                            value={content}
+                            placeholder='your support question'
+                            onChange={(e) => setContent(e.target.value)}
+                            required
+                            className='enquiries-text-box'
+                            disabled={isLoading}
+                        />
+                    </div>
+                    <button 
+                        type="submit" 
                         disabled={isLoading}
-                    />
-                </div>
-                <button 
-                    type="submit" 
-                    disabled={isLoading}
-                >{isLoading ? 'Submitting' : 'Submit'}</button>
-            </form>
+                    >{isLoading ? 'Submitting' : 'Submit'}</button>
+                </form>
+            </div>
+        </div>
         </div>
     )
 }
