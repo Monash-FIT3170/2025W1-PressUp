3e364bcd0dd2cb803bdbc889afa6c324f3d8c096
kbis0003 <kbis0003@student.monash.edu>
2025-08-10 18:03:02 +1000

    add enquiry response page for staff

---
+++ imports/ui/Components/CustomerCommunication/EnquiryResponsePage.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useSubscribe, useFind } from "meteor/react-meteor-data";
+import { EnquiriesCollection } from "../../../api/enquiries/enquiries-collection";
+import { LoadingIndicator } from "../OrderSummary/LoadingIndicator/LoadingIndicator.jsx";
+import './EnquiryResponseCard.css';
+
+export const EnquiryResponsePage = (() => {
+    
+})
