0ecd67439d5bf666fc15ef9ea6a6ac9177a203a1
kbis0003 <kbis0003@student.monash.edu>
2025-08-11 01:32:41 +1000

    enquiry response card now functional. still needs styling and to be able to save a draft response.

--- imports/ui/Components/CustomerCommunication/EnquiryResponseCard.jsx
+++ imports/ui/Components/CustomerCommunication/EnquiryResponseCard.jsx
@@ -10,13 +10,20 @@ export const EnquiryResponseCard = ({
     enquiryID
 }) => {
     const isLoading = useSubscribe("enquiries.id", enquiryID);
-    var enquiry = useFind(()=>EnquiriesCollection.find({_id: enquiryID}),[enquiryID]);]
+    var enquiry = useFind(()=>EnquiriesCollection.find({_id: enquiryID}),[enquiryID]);
     enquiry = enquiry[0];
     
-    const [response,setResponse] = useState(enquiry.response)
+    const [response,setResponse] = useState(enquiry.response);
+    const [inFocus,setInFocus] = useState(false);
 
     if (isLoading()) {
         return <LoadingIndicator />;
+    }
+
+    const handleFocus = (e) => {
+        console.log("hello")
+        e.preventDefault();
+        setInFocus(!inFocus);
     }
 
     const handleSave = (e) => {
@@ -29,7 +36,37 @@ export const EnquiryResponseCard = ({
         Meteor.call("enquiry.respond",enquiryID,response);
     }
 
+    if (!enquiry.active) {
+        return (<></>)
+    }
+
+
+    if (inFocus) {
+        return (
+            <div>
+            <div> {enquiry.name ? enquiry.name : "Anonymous"} asked: <p>{enquiry.content} </p></div>
+            <form onSubmit={handleSubmit}>
+                <div className="enquiries-input-group">
+                        <label htmlFor="response">Response</label>
+                        <textarea
+                            id="response"
+                            type="text"
+                            value={response}
+                            placeholder='the response'
+                            onChange={(e) => setResponse(e.target.value)}
+                            required
+                        />
+                </div>
+                <button 
+                        type="submit"
+                    >answer</button>
+            </form> 
+            </div>
+        )
+    }
     return (
-        <div>{enquiry.content}</div>
+        <div>
+        <button onClick={handleFocus}> {enquiry.name ? enquiry.name : "Anonymous"} asked: <p>{enquiry.content}</p></button>       
+        </div>
     )
 }
