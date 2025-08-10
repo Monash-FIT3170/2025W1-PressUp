5dc70e0284d5d8781333b92caeb55fdc3bcbf7b7
kbis0003 <kbis0003@student.monash.edu>
2025-08-10 14:04:16 +1000

    reformatted confirmation email

--- imports/api/enquiries/enquiries-methods.js
+++ imports/api/enquiries/enquiries-methods.js
@@ -4,14 +4,25 @@ import { EnquiriesCollection } from "./enquiries-collection";
 
 Meteor.methods({
   async "enquiry.insert"(enqContact,enqContent) {
-    Email.sendAsync({to:enqContact,from:"donotreply.pressup@gmail.com",subject:"Thank you for reaching out",text:"we have recieved your enquiry and will respond as soon as possible.\n\nyour enquiry: \n'"+enqContent+"'"})
     const enquiry = {
         content: enqContent,
         contact:enqContact,
         date:Date.now(),
-        active: true
+        active: true,
+        response: '',
     };
-    return await EnquiriesCollection.insertAsync(enquiry);
+    const result = await EnquiriesCollection.insertAsync(enquiry);
+    Email.sendAsync({to:enqContact,from:"donotreply.pressup@gmail.com",subject:"Thank you for reaching out",html:`
+    <h1>We have received your enquiry and will get back to you as soon as possible.</h1>
+    <p><strong>Enquiry ID: </strong><em>` + result +`</em></p>
+    <p>Your enquiry: </p><p><em>`+enqContent+`</em></p>
+    <footer>
+      <p>Best regards,</p>
+      <p>the PressUp Team</p>
+    </footer>
+  `})
+    
+    return result
   },
   async "enquiry.respond"(id,answer) {
     enquiry = await EnquiriesCollection.find({_id:id})
