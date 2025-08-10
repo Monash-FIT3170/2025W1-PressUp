b5b18b0f84c4f53da60fcfd48213888a6635e523
kbis0003 <kbis0003@student.monash.edu>
2025-08-10 18:15:55 +1000

    work on enquiry response UI for testing.

---
+++ imports/ui/Components/CustomerCommunication/EnquiryResponseCard.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useSubscribe, useFind } from "meteor/react-meteor-data";
+import { EnquiriesCollection } from "../../../api/enquiries/enquiries-collection";
+import { LoadingIndicator } from "../OrderSummary/LoadingIndicator/LoadingIndicator.jsx";
+import './EnquiryResponseCard.css';
+
+
+export const EnquiryResponseCard = ({
+    enquiryID
+}) => {
+    const isLoading = useSubscribe("enquiries.id", enquiryID);
+    const enquiry = useFind(()=>EnquiriesCollection.find({ enquiryID }),[enquiryID]);
+    enquiry = enquiry[0]
+    const [response,setResponse] = useState(enquiry.response)
+
+    if (isLoading()) {
+        return <LoadingIndicator />;
+    }
+
+    const handleSave = (e) => {
+        e.preventDefault();
+        Meteor.call("enquiry.draft",enquiryID,response);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        Meteor.call("enquiry.respond",enquiryID,response);
+    }
+
+    return (
+        <div>{enquiry.content}</div>
+    )
+}
--- imports/ui/Components/CustomerCommunication/EnquiryResponsePage.jsx
+++ imports/ui/Components/CustomerCommunication/EnquiryResponsePage.jsx
@@ -4,7 +4,19 @@ import { useSubscribe, useFind } from "meteor/react-meteor-data";
 import { EnquiriesCollection } from "../../../api/enquiries/enquiries-collection";
 import { LoadingIndicator } from "../OrderSummary/LoadingIndicator/LoadingIndicator.jsx";
 import './EnquiryResponseCard.css';
+import { EnquiryResponseCard } from './EnquiryResponseCard.jsx';
 
 export const EnquiryResponsePage = (() => {
-    
+    const isLoading = useSubscribe("enquiries.active");
+    const enquiries = useFind(()=>EnquiriesCollection.find({ active }),[true]);
+
+    return (
+        <div>
+            {enquiries.map((enquiry)=>(
+                <EnquiryResponseCard 
+                    enquiryID={enquiry._id}
+                />
+            ))}
+        </div>
+    )
 })
