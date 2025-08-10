fba7f30f23b6814861e044ed5b800667e6cc4167
kbis0003 <kbis0003@student.monash.edu>
2025-08-10 11:05:45 +1000

    add css for enquiries page and fix insertion method.

--- imports/api/enquiries/enquiries-methods.js
+++ imports/api/enquiries/enquiries-methods.js
@@ -3,8 +3,14 @@ import { Email } from "meteor/email";
 import { EnquiriesCollection } from "./enquiries-collection";
 
 Meteor.methods({
-  async "enquiry.insert"(enquiry) {
-    Email.sendAsync({to:enquiry.contact,from:"donotreply.pressup@gmail.com",subject:"Thank you for reaching out",text:"we have recieved your enquiry and will respond as soon as possible."})
+  async "enquiry.insert"(enqContact,enqContent) {
+    Email.sendAsync({to:enqContact,from:"donotreply.pressup@gmail.com",subject:"Thank you for reaching out",text:"we have recieved your enquiry and will respond as soon as possible.\n\nyour enquiry: \n'"+enqContent+"'"})
+    const enquiry = {
+        content: enqContent,
+        contact:enqContact,
+        date:Date.now(),
+        active: true
+    };
     return await EnquiriesCollection.insertAsync(enquiry);
   },
   async "enquiry.respond"(id,answer) {
--- imports/ui/Enquiries/enquiries.jsx
+++ imports/ui/Enquiries/enquiries.jsx
@@ -5,44 +5,55 @@ import './Enquiries.css';
 export const Enquiries = () => {
     const [contact,setContact] = useState('');
     const [content,setContent] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
     const checkIsEmail = (string) => {
         emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
         return emailRegex.test(string);
     }
     const handleSubmit = (e) => {
+        setIsLoading(true);
         e.preventDefault();
         if (checkIsEmail(contact) && content != '') {
-            console.log(contact);
-            console.log(content);
-            //Meteor.call("enquiry.insert",contact,content);
+            Meteor.call("enquiry.insert",contact,content,(err) => {
+                setIsLoading(false)
+            });
             setContact('');
             setContent('');
         }
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="contact">Email</label>
-                <input
-                    id="contact"
-                    type="email"
-                    value={contact}
-                    onChange={(e) => setContact(e.target.value)}
-                    placeholder="Enter Email"
-                    required
-                />
-                <label htmlFor="content">How can we help you?</label>
-                <input
-                    id="content"
-                    type="text"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                    required
-                />
+        <div className="enquiries-container">
+            <form onSubmit={handleSubmit} className="enquiries-form">
+                <div className="enquiries-input-group">
+                    <label htmlFor="contact">Email</label>
+                    <input
+                        id="contact"
+                        type="email"
+                        value={contact}
+                        onChange={(e) => setContact(e.target.value)}
+                        placeholder="Enter Email"
+                        required
+                        disabled={isLoading}
+                    />
+                </div>
+                <div className="enquiries-input-group">
+                    <label htmlFor="content">How can we help you?</label>
+                    <textarea
+                        id="content"
+                        type="text"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        required
+                        className='enquiries-text-box'
+                        disabled={isLoading}
+                    />
+                </div>
                 <button 
                     type="submit" 
-                />
+                    disabled={isLoading}
+                >{isLoading ? 'Submitting' : 'Submit'}</button>
             </form>
         </div>
     )
